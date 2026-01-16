a60c9730ca864ef5b755a993a9ac43f34338bb85
mjennings754 <mitchellj@outlook.jp>
2026-01-17 08:43:09 +1100

    add selection sort

---
+++ selection_sort/main.py
@@ -0,0 +1,14 @@
+C = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+selection_sort(C)
+print(C)
